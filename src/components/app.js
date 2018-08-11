822dc33b428f8219d14e9c60a41621c51b4171b1
jordanhudgens <jordan@devcamp.com>
2018-08-11 12:01:00 -0600

    Built out the navigation bar

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Link
+} from "react-router-dom";
 
 import Home from "./views/home";
 import About from "./views/about";
@@ -7,9 +13,24 @@ import NoMatch from "./views/no-match";
 
 const NavLinks = () => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About Me</Link>
+    <div className="nav-wrapper">
+      <div className="left-side">
+        <div className="nav-link-wrapper">
+          <NavLink exact to="/" activeClassName="active-nav-link">
+            Home
+          </NavLink>
+        </div>
+
+        <div className="nav-link-wrapper">
+          <NavLink to="/about" activeClassName="active-nav-link">
+            About Me
+          </NavLink>
+        </div>
+      </div>
+
+      <div className="left-side">
+        <div className="brand">JORDAN HUDGENS</div>
+      </div>
     </div>
   );
 };
@@ -17,7 +38,7 @@ const NavLinks = () => {
 export default class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div className="container">
         <Router>
           <div>
             <NavLinks />
