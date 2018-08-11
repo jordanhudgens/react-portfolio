c1dfc4aa4fc7632d687064d5fba243cf624d8ae4
jordanhudgens <jordan@devcamp.com>
2018-08-11 10:57:19 -0600

    Created a functional component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+const NavLinks = () => {
+  return (
+    <div>
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+    </div>
+  );
+};
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+      <div className="app">
+        <NavLinks />
+        <h1>Portfolio Application</h1>
       </div>
     );
   }
