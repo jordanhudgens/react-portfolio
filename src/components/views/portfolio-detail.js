74f38967770c6d9854596b5387afa9e1cee173ac
jordanhudgens <jordan@devcamp.com>
2018-08-20 16:01:47 -0700

    Added a reader for the portfolio detail componet

--- src/components/views/portfolio-detail.js
+++ src/components/views/portfolio-detail.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class PortfolioDetail extends Component {
   render() {
-    return <div>Portfolio Detail</div>;
+    return <div>{this.props.match.params.slug}</div>;
   }
 }
