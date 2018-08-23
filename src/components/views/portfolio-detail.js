caebd9b3442e46f213c16c2eb77939d5873915f0
jordanhudgens <jordan@devcamp.com>
2018-08-23 16:35:07 -0700

    Started building out the styles for the portfolio detail component

--- src/components/views/portfolio-detail.js
+++ src/components/views/portfolio-detail.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 
+import portfolioItemData from "../../data/portfolio-items.json";
+
 export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { portfolioItem: null };
+  }
+  componentWillMount() {
+    const data = portfolioItemData.portfolioItems;
+    const portfolioItem = data.find(el => {
+      return String(el.id) === this.props.match.params.slug;
+    });
+
+    this.setState({ portfolioItem });
+  }
+
   render() {
+    const { id, bgImage, logo, name, description } = this.state.portfolioItem;
+
     return (
       <div className="portfolio-detail-wrapper">
-        <div className="banner">{this.props.match.params.slug}</div>
+        <div className="banner">
+          <img src={bgImage} />
+
+          <div className="portfolio-detail-title">{name}</div>
+        </div>
       </div>
     );
   }
