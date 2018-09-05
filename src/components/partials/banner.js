18feeded34078d256f754f562b69af8249930f56
jordanhudgens <jordan@devcamp.com>
2018-09-05 16:42:57 -0500

    Finalized styles for the portfolio details

--- src/components/partials/banner.js
+++ src/components/partials/banner.js
@@ -3,7 +3,11 @@ import React from "react";
 export default function(props) {
   return (
     <div className="bottom-content-wrapper">
-      <a href={props.bannerLink} className="btn btn__white btn__lg">
+      <a
+        href={props.bannerLink}
+        className="btn btn__white btn__lg"
+        target="_blank"
+      >
         {props.name}
       </a>
     </div>
