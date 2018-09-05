594f1178cfaa6117c946ecd3ac43e327bd7c4973
jordanhudgens <jordan@devcamp.com>
2018-09-05 16:53:18 -0500

    Added the placeholder for the new blog component

--- src/components/app.js
+++ src/components/app.js
@@ -12,6 +12,7 @@ import About from "./views/about";
 import PortfolioDetail from "./views/portfolio-detail";
 import Blog from "./views/blog";
 import BlogDetail from "./views/blog-detail";
+import NewBlog from "./views/new-blog";
 import NoMatch from "./views/no-match";
 import NavLinks from "./partials/navigation";
 
@@ -26,6 +27,7 @@ export default class App extends Component {
               <Route exact path="/" component={Home} />
               <Route path="/about" component={About} />
               <Route path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route path="/blogs/new" component={NewBlog} />
               <Route path="/blogs" component={Blog} />
               <Route path="/blog/:slug" component={BlogDetail} />
               <Route component={NoMatch} />
