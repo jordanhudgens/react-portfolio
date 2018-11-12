7bbb6fe38aa13b3a8d0a526b115705d8cb30db29
jordanhudgens <jordan@devcamp.com>
2018-11-12 10:34:58 -0700

    Added the ability to upload thumb images

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -7,29 +7,42 @@ export default class PortfolioForm extends Component {
     this.state = {
       name: "",
       description: "",
-      link: ""
+      link: "",
+      thumb_image: ""
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.buildForm = this.buildForm.bind(this);
   }
 
   handleChange(event) {
-    console.log("handle change", event);
-    this.setState({ [event.target.name]: event.target.value });
+    if (event.target.files) {
+      this.setState({ [event.target.name]: event.target.files[0] });
+    } else {
+      this.setState({ [event.target.name]: event.target.value });
+    }
+  }
+
+  buildForm() {
+    let formData = new FormData();
+
+    formData.append("portfolio_item[name]", this.state.name);
+    formData.append("portfolio_item[description]", this.state.description);
+    formData.append("portfolio_item[link]", this.state.link);
+
+    if (this.state.thumb_image) {
+      formData.append("portfolio_item[thumb_image]", this.state.thumb_image);
+    }
+
+    return formData;
   }
 
   handleSubmit(event) {
     axios
       .post(
         "https://jordan.devcamp.space/portfolio/portfolio_items",
-        {
-          portfolio_item: {
-            name: this.state.name,
-            description: this.state.description,
-            url: this.state.link
-          }
-        },
+        this.buildForm(),
         { withCredentials: true }
       )
       .then(response => {
@@ -75,6 +88,13 @@ export default class PortfolioForm extends Component {
             placeholder="Link"
             value={this.state.link}
             onChange={this.handleChange}
+          />
+
+          <input
+            type="file"
+            name="thumb_image"
+            onChange={this.handleChange}
+            multiple
           />
 
           <button type="submit">Save</button>
