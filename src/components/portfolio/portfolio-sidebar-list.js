1ea32447bdf2bacc776ba0e7ed89d2c869721fb9
jordanhudgens <jordan@devcamp.com>
2018-12-05 12:29:59 -0700

    Added new styles for the portfolio manager

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -29,9 +29,16 @@ export default class PortfolioSidebarList extends Component {
 
   render() {
     const portfolioList = this.state.portfolioItems.map(portfolioItem => {
+      console.log("portfolio list items", portfolioItem);
       return (
         <div className="portfolio-item-thumb" key={portfolioItem.id}>
-          {portfolioItem.name}
+          <div className="thumb-img">
+            <img src={portfolioItem.thumb_image_url} />
+          </div>
+
+          <h2>{portfolioItem.id}</h2>
+
+          <div className="title">{portfolioItem.name}</div>
         </div>
       );
     });
