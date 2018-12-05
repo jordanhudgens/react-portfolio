8a5869dad274a87271d5dd3cf908d10056ddb84a
jordanhudgens <jordan@devcamp.com>
2018-12-05 12:11:26 -0700

    Built out select form element for categories

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -98,13 +98,15 @@ export default class PortfolioForm extends Component {
               onChange={this.handleChange}
             />
 
-            <input
-              type="text"
+            <select
               name="category"
-              placeholder="Category"
               value={this.state.category}
               onChange={this.handleChange}
-            />
+            >
+              <option value="eCommerce">eCommerce</option>
+              <option value="Scheduling">Scheduling</option>
+              <option value="Enterprise">Entperise</option>
+            </select>
           </div>
 
           <div className="one-column">
