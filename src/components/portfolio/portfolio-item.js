17187ef2294902f7e585447bf68e04e96e634d29
jordanhudgens <jordan@devcamp.com>
2019-01-11 14:56:06 -0600

    Updated data call to the live API instead of JSON file for the homepage

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -6,7 +6,7 @@ export default class PortfolioItem extends Component {
     super(props);
 
     this.state = {
-      portfolioItemClass: null
+      portfolioItemClass: ""
     };
   }
 
@@ -15,38 +15,34 @@ export default class PortfolioItem extends Component {
   }
 
   handleMouseLeave() {
-    this.setState({ portfolioItemClass: null });
+    this.setState({ portfolioItemClass: "" });
   }
 
   render() {
-    const { id, bgImage, logo, name, description } = this.props.portfolioItem;
-    const portfolioDetailPath = `/portfolio/${id}`;
-
+    const { id, description, thumb_image_url, logo_url } = this.props.item;
     return (
-      <Link to={portfolioDetailPath}>
+      <div
+        className="portfolio-item-wrapper"
+        onMouseEnter={() => this.handleMouseEnter()}
+        onMouseLeave={() => this.handleMouseLeave()}
+      >
         <div
-          className="portfolio-item-wrapper"
-          onMouseEnter={() => this.handleMouseEnter()}
-          onMouseLeave={() => this.handleMouseLeave()}
-        >
-          <div
-            className={
-              "portfolio-img-background " + this.state.portfolioItemClass
-            }
-            style={{
-              backgroundImage: "url(" + bgImage + ")"
-            }}
-          />
+          className={
+            "portfolio-img-background " + this.state.portfolioItemClass
+          }
+          style={{
+            backgroundImage: "url(" + thumb_image_url + ")"
+          }}
+        />
 
-          <div className="img-text-wrapper">
-            <div className="logo-wrapper">
-              <img src={logo} alt={name} />
-            </div>
-
-            <div className="subtitle">{description}</div>
+        <div className="img-text-wrapper">
+          <div className="logo-wrapper">
+            <img src={logo_url} />
           </div>
+
+          <div className="subtitle">{description}</div>
         </div>
-      </Link>
+      </div>
     );
   }
 }
--- src/components/portfolio/portfolio-list.js
+++ src/components/portfolio/portfolio-list.js
@@ -1,19 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
 import PortfolioItem from "./portfolio-item";
-import portfolioItemData from "../../data/portfolio-items.json";
 
-const PortfolioList = () => {
-  const data = portfolioItemData.portfolioItems;
-  const portfolioListItems = data.map(item => {
-    return <PortfolioItem key={item.id} portfolioItem={item} />;
-  });
+export default class PortfolioList extends Component {
+  constructor() {
+    super();
 
-  return (
-    <div className="content-wrapper">
-      <div className="portfolio-items-wrapper">{portfolioListItems}</div>
-    </div>
-  );
-};
+    this.state = {
+      isLoading: false,
+      data: []
+    };
 
-export default PortfolioList;
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://jordan.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        this.setState({
+          data: response.data.portfolio_items
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return <PortfolioItem key={item.id} item={item} />;
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div className="portfolio-items-wrapper">
+        <button className="btn" onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
+
+        {this.portfolioItems()}
+      </div>
+    );
+  }
+}
