46f3a7bad21e372410c7264467048ffa85d17fcd
jordanhudgens <jordan@devcamp.com>
2019-01-18 11:45:45 -0600

    Built out initial functionality for calling and rendering blog posts

--- src/components/views/blog.js
+++ src/components/views/blog.js
@@ -1,7 +1,37 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Blog extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      blogItems: []
+    };
+  }
+
+  componentDidMount() {
+    this.getBlogItems();
+  }
+
+  getBlogItems() {
+    axios
+      .get("https://jordan.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({ blogItems: [...response.data.portfolio_blogs] });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
-    return <div>Blog</div>;
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <div>{blogItem.title}</div>;
+    });
+
+    return <div className="blog-list-wrapper">{blogRecords}</div>;
   }
 }
