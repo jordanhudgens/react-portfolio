9acd80b592eec5515b9257d83a3ad784ca3a6eb8
jordanhudgens <jordan@devcamp.com>
2019-01-21 15:31:38 -0700

    Added initial infinite scroll feature

--- src/components/views/blog.js
+++ src/components/views/blog.js
@@ -8,21 +8,59 @@ export default class Blog extends Component {
     super();
 
     this.state = {
+      error: false,
+      hasMore: true,
+      totalCount: 0,
+      isLoading: false,
+      currentPage: 1,
       blogItems: []
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+
+    window.onscroll = () => {
+      const {
+        getBlogItems,
+        state: { error, isLoading, totalCount, blogItems, currentPage }
+      } = this;
+
+      if (error || isLoading || totalCount === blogItems.length) return;
+
+      // Checks that the page has scrolled to the bottom
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        this.setState(
+          {
+            currentPage: currentPage + 1
+          },
+          getBlogItems()
+        );
+      }
     };
   }
 
-  componentDidMount() {
+  componentWillMount() {
     this.getBlogItems();
   }
 
   getBlogItems() {
+    console.log("current state", this.state);
     axios
-      .get("https://jordan.devcamp.space/portfolio/portfolio_blogs", {
-        withCredentials: true
-      })
+      .get(
+        `https://jordan.devcamp.space/portfolio/portfolio_blogs?page=${this
+          .state.currentPage}`,
+        {
+          withCredentials: true
+        }
+      )
       .then(response => {
-        this.setState({ blogItems: [...response.data.portfolio_blogs] });
+        console.log("Getting more...", response);
+        this.setState({
+          blogItems: [...response.data.portfolio_blogs],
+          totalCount: response.data.meta.total_records
+        });
       })
       .catch(error => {
         console.log(error);
@@ -30,7 +68,6 @@ export default class Blog extends Component {
   }
 
   // TODO
-  // Add banner image to blog detail component
   // Integrate infinite scroll into front end
   // Integrate editable input
   // Integrate editable textarea
